ebcac913d6ab96a8bb44b4e4fb2c97985ab642c9
sidit77 <sidit77@gmail.com>
2023-05-23 07:13:49 +0200

    finished lib version

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
-mod clipboard;
-mod error;
-mod hotkey;
-mod input;
-mod messages;
+use anyhow::Result;
+use enigo::{Enigo, Key, KeyboardControllable};
+use tao::accelerator::Accelerator;
+use tao::clipboard::Clipboard;
+use tao::event::Event;
+use tao::event_loop::{ControlFlow, EventLoop};
+use tao::global_shortcut::ShortcutManager;
+use tao::keyboard::KeyCode;
+use tao::platform::run_return::EventLoopExtRunReturn;
 
-use windows_sys::Win32::UI::Input::KeyboardAndMouse::{MOD_NOREPEAT, VK_INSERT};
+fn main() -> Result<()> {
+    let mut event_loop = EventLoop::new();
+    let mut hotkey_manager = ShortcutManager::new(&event_loop);
+    let accelerator = Accelerator::new(None, KeyCode::Insert);
+    let _global_shortcut = hotkey_manager.register(accelerator.clone())?;
+    let clipboard = Clipboard::new();
+    let mut enigo = Enigo::new();
 
-use crate::clipboard::Clipboard;
-use crate::error::WinResult;
-use crate::hotkey::GlobalHotkey;
-use crate::input::type_string;
-use crate::messages::{run_event_loop, Event};
-
-const POWER_PASTE: i32 = 0x0001;
-
-fn main() -> WinResult<()> {
-    let _hotkey = GlobalHotkey::register(POWER_PASTE, MOD_NOREPEAT, VK_INSERT)?;
-    run_event_loop(|event| {
-        if let Event::Hotkey(POWER_PASTE) = event {
-            let text = Clipboard::new()?.get_text()?;
-            type_string(&text)?;
+    event_loop.run_return(move |event, _, flow| {
+        *flow = ControlFlow::Wait;
+        match event {
+            Event::GlobalShortcutEvent(id) if id == accelerator.clone().id() => {
+                if let Some(text) = clipboard.read_text() {
+                    let mut first = true;
+                    for line in text.lines() {
+                        if !first {
+                            enigo.key_click(Key::Return);
+                        }
+                        enigo.key_sequence(line);
+                        first = false;
+                    }
+                }
+            }
+            _ => {}
         }
-        Ok(())
-    })?;
+    });
     Ok(())
 }
